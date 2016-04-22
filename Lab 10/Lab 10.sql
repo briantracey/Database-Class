6245f986dc715ee49c70901646150a88a2be479c
briantracey <brian.tracey1@marist.edu>
2016-04-22 12:56:39 -0400

    Lab 10; Brian Tracey; 4.19.2016
    Lab 10 due April 19th 2016 by Brian Tracey writing some cool stored
    procedures.

---
+++ Lab 10/Lab 10.sql
@@ -0,0 +1,48 @@
+﻿--Brian Tracey
+--Last Lab! 10!
+
+
+CREATE OR REPLACE FUNCTION preReqsFor(int, REFCURSOR)
+RETURNS REFCURSOR AS
+$$
+DECLARE
+    course int  := $1;
+    resultset REFCURSOR := $2;
+BEGIN
+    OPEN RESULTSET FOR
+        SELECT num, name, credits
+        FROM   Courses
+        JOIN   Prerequisites
+        ON     Courses.num = Prerequisites.preReqNum
+        WHERE  Prerequisites.courseNum = course;
+    RETURN resultset;
+END;
+$$
+language plpgsql;
+
+select preReqsFor(308,'results');
+Fetch all from results;
+
+
+
+
+CREATE OR REPLACE FUNCTION isPreReqFor(int, REFCURSOR)
+RETURNS REFCURSOR AS
+$$
+DECLARE
+    course int := $1;
+    resultset REFCURSOR := $2;
+BEGIN
+    OPEN RESULTSET FOR
+        SELECT num, name, credits
+        FROM Courses
+        JOIN Prerequisites
+        ON Courses.num = Prerequisites.courseNum
+        WHERE Prerequisites.preReqNum = course;
+    RETURN resultset;
+END;
+$$
+language plpgsql;
+
+select isPreReqFor(220, 'results');
+Fetch all from results;
